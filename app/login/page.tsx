5098357a9364ddee25b7b928bde6f48d97c777e0
adrielle-b <adriielle@gmail.com>
2024-05-27 11:44:36 -0300

    initial commit

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import React, { useState } from 'react';
+
+export default function Login() {
+
+    return (
+      <div>
+        <h1>Blog</h1>
+        <h2>Login</h2>
+      </div>
+    );
+  }
